5ae237d68cbb186ec745ea9bc01aad744050acec
sakoh <sakohartounian@yahoo.com>
2016-07-05 15:54:48 -0700

    jobs chart added

--- js/pipeline.js
+++ js/pipeline.js
@@ -2,19 +2,24 @@ $(document).ready(function(){
 	var ctx = $(this).find('#pipeline');
 
 	var pipelineChart = new Chart(ctx, {
-    	type: 'bar',
+    	type: 'horizontalBar',
     	data: {
-        	labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        	labels: [
+				"Extension Pending",
+				"Ending Next 15 Days",
+				"Active Headcount",
+				"Filled Jobs",
+				"Open Jobs"
+			],
         	datasets: [{
-            	label: '# of Votes',
-            	data: [12, 19, 3, 5, 2, 3],
+            	label: '# of Jobs',
+            	data: [42, 0, 1138, 1279, 518],
             	backgroundColor: [
                 	'rgba(255, 99, 132, 0.2)',
                 	'rgba(54, 162, 235, 0.2)',
                 	'rgba(255, 206, 86, 0.2)',
                 	'rgba(75, 192, 192, 0.2)',
-                	'rgba(153, 102, 255, 0.2)',
-                	'rgba(255, 159, 64, 0.2)'
+                	'rgba(153, 102, 255, 0.2)'
             	],
             	borderColor: [
                 	'rgba(255,99,132,1)',
