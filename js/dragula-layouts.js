27226eab8e55b048c2f7b815fde1239e556a9a35
sakoh <sakohartounian@yahoo.com>
2016-07-05 19:10:47 -0700

    panels now draggable

--- js/dragula-layouts.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	dragula([document.getElementById("left-pane"), document.getElementById("right-pane")], {
-	  moves: function (el, container, handle) {
-    	return handle.className === 'handle';
-	  }
-	});
-});
